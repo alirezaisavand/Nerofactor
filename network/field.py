0331f81057dd4cc7c6d5c27222e50936dfd46f20
alirezaisavand <isavand.alireza@gmail.com>
2025-01-21 18:52:11 -0600

    remove duplicate definition of Embedder (from nerfactor and NeRO) in field.py

--- network/field.py
+++ network/field.py
@@ -715,7 +715,7 @@ def saturate_dot(v0, v1):
 
 from nerfactor.third_party.xiuminglib import xiuminglib as xm
 from nerfactor.nerfactor.models.brdf import Model as BRDFModel
-from nerfactor.nerfactor.networks.embedder import Embedder
+from nerfactor.nerfactor.networks.embedder import Embedder as nerfactor_Embedder
 from nerfactor.nerfactor.util import config as configutil, \
     io as ioutil
 
