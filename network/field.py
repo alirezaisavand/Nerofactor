da31a9a8d7f159b6b9295e149846930b74821aa2
alirezaisavand <isavand.alireza@gmail.com>
2025-01-23 04:51:53 -0600

    debug devices

--- network/field.py
+++ network/field.py
@@ -1069,6 +1069,7 @@ class MCShadingNetwork(nn.Module):
 
         def chunk_func(surf):
             surf_embed = embedder(surf)
+            print('surf embed, surf, mlp_layers, out_layer, embedder', surf_embed.device, surf.device, mlp_layers.device, out_layer.device, embedder.device)
             brdf_z = out_layer(mlp_layers(surf_embed))
             return brdf_z
 
