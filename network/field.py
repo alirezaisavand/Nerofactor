b219489ea8f384538f5900b9a0e40720fbe76e8f
alirezaisavand <isavand.alireza@gmail.com>
2025-01-30 17:52:27 -0600

    debugging specular part

--- network/field.py
+++ network/field.py
@@ -1284,7 +1284,8 @@ class MCShadingNetwork(nn.Module):
 
         black_count = (spec_brdf == 0).all(dim=1).sum().item()
         if not is_train:
-            print('pts size:', len(spec_brdf), 'number of zeros:', black_count)
+            print('pts size:', len(pts))
+            print('brdf size:', len(spec_brdf), 'number of zeros:', black_count)
             print('black brdf props:', brdf_prop[(spec_brdf == 0).all(dim=1)])
 
         # specular_colors = torch.mean(fresnel * specular_lights, 1)
