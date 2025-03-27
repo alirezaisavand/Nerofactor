7cd3e7d6c191181e498e13fa67712be21a10a3de
alirezaisavand <isavand.alireza@gmail.com>
2025-03-26 19:38:02 -0600

    debug segmentation

--- network/renderer.py
+++ network/renderer.py
@@ -1013,22 +1013,26 @@ class NeROMaterialRenderer(nn.Module):
     # This part is for generating segmentation masks
     def _construct_nerf_segmentation_masks(self, imgs_info, device='cpu', is_train=True):
         imn, _, h, w = imgs_info['imgs'].shape
-        coords = torch.stack(torch.meshgrid(torch.arange(h), torch.arange(w)), -1)[:, :, (1, 0)]  # h,w,2
-        coords = coords.to('cpu')
-        coords = coords.float()[None, :, :, :].repeat(imn, 1, 1, 1)  # imn,h,w,2
-        coords = coords.reshape(imn, h * w, 2)
-        coords = torch.cat([coords + 0.5, torch.ones(imn, h * w, 1, dtype=torch.float32, device='cpu')], 2)  # imn,h*w,3
 
-        # imn,h*w,3 @ imn,3,3 => imn,h*w,3
-        rays_d = coords @ torch.inverse(imgs_info['Ks'][0]).permute(0, 2, 1)
+        i, j = torch.meshgrid(torch.linspace(0, w - 1, w),
+                              torch.linspace(0, h - 1, h))  # pytorch's meshgrid has indexing='ij'
+        i = i.t()
+        j = j.t()
+
+        K = imgs_info['Ks'][0]
+        dirs = torch.stack([(i - K[0][2]) / K[0][0], -(j - K[1][2]) / K[1][1], -torch.ones_like(i)], -1)
+
+        imgs = imgs_info['imgs'].permute(0, 2, 3, 1).reshape(imn, h * w, 3)  # imn,h*w,3
         poses = imgs_info['poses']  # imn,3,4
-        R, t = poses[:, :, :3], poses[:, :, 3:]
-        rays_d = rays_d @ R
-        rays_d = F.normalize(rays_d, dim=-1)
-        rays_o = -R.permute(0, 2, 1) @ t  # imn,3,3 @ imn,3,1
-        self._warn_ray_tracing(rays_o)
-        rays_o = rays_o.permute(0, 2, 1).repeat(1, h * w, 1)  # imn,h*w,3
+        # if is_train:
+        #     masks = imgs_info['masks'].reshape(imn, h * w)
 
+        rays_d = [torch.sum(dirs[..., None, :].cpu() * poses[i, :3, :3], -1) for i in range(imn)]
+        rays_d = torch.stack(rays_d, 0).reshape(imn, h * w, 3)
+        rays_o = [poses[i, :3, -1].expand(rays_d[0].shape) for i in range(imn)]
+        rays_o = torch.stack(rays_o, 0).reshape(imn, h * w, 3)
+        self._warn_ray_tracing(rays_o)
+        poses = poses.unsqueeze(1).repeat(1, h * w, 1, 1)
         rgb = imgs_info['imgs'].reshape(imn, 3, h * w).permute(0, 2, 1)  # imn,h*w,3
 
         from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
