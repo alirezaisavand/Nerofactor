61888af1379d2494e3ca6452685d06e9f1952122
alirezaisavand <isavand.alireza@gmail.com>
2025-03-26 19:34:04 -0600

    convert all_imgs_info to torch

--- network/renderer.py
+++ network/renderer.py
@@ -872,6 +872,7 @@ class NeROMaterialRenderer(nn.Module):
         self.train_ids = np.asarray(self.train_ids)
         # This part is for genetaring sementation masks
         all_imgs_info = build_imgs_info(self.database, np.asarray(self.database.get_img_ids()), self.is_nerf)
+        all_imgs_info = imgs_info_to_torch(all_imgs_info, 'cpu')
         self.seg_masks = self._construct_nerf_segmentation_masks(all_imgs_info)
         print('segmentation masks are created')
         print('seg_masks shape:', self.seg_masks.shape)
