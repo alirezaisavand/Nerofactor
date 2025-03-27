c1092ef4c0acae744ecdf3e7e0890525eaf465c0
alirezaisavand <isavand.alireza@gmail.com>
2025-03-26 21:25:08 -0600

    debug images shape

--- network/renderer.py
+++ network/renderer.py
@@ -1141,7 +1141,9 @@ class NeROMaterialRenderer(nn.Module):
 
         # For image 0, assume you have a manually selected mask (or one chosen via seg_model).
         print('imgs[0].shape', imgs[0].shape)
-        src_mask = seg_model.generate(imgs[0])[2]['segmentation']
+        src_masks = seg_model.generate(imgs[0])
+        print('src_masks.shape', src_masks.shape)
+        src_mask = src_masks[2]['segmentation']
         # Here, we assume src_mask is the binary mask of the target object.
         selected_masks.append(src_mask)
 
