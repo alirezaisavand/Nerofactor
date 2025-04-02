a2fe8ade526e06897ff393bb7de18dd28f543620
alirezaisavand <isavand.alireza@gmail.com>
2025-04-02 17:34:08 -0600

    debug projection function from 3D to 2D

--- network/renderer.py
+++ network/renderer.py
@@ -1210,7 +1210,7 @@ class NeROMaterialRenderer(nn.Module):
         print('camera_poses shape:', camera_poses.shape)
         print('Ks shape:', Ks.shape)
 
-        for i in range(0, n):
+        for i in range(0, 1):
             # Get segmentation masks for image i.
             seg_masks = seg_model.generate(imgs[i])
             # Use the previously computed pointcloud to find the best match.
