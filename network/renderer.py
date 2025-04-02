0232a819fb25ccf151691589c98c7753e86dffc6
alirezaisavand <isavand.alireza@gmail.com>
2025-04-02 00:33:14 -0600

    debug projection function from 3D to 2D

--- network/renderer.py
+++ network/renderer.py
@@ -1063,19 +1063,19 @@ class NeROMaterialRenderer(nn.Module):
           z:  (N,) depth values in camera space.
         """
 
-        R = pose[:, :3]
-        t = pose[:, 3]
+        # R = pose[:, :3]
+        # t = pose[:, 3]
         # Transform points to camera coordinate system
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        R = R.to(device)
-        t = t.to(device)
+        # R = R.to(device)
+        # t = t.to(device)
         pts = pts.to(device)
 
-        R_inv = R.transpose(0, 1)  # R_inv = R^T
-        points_cam = (pts - t.unsqueeze(0)) @ R_inv  # (N, 3)
-
+        # R_inv = R.transpose(0, 1)  # R_inv = R^T
+        # points_cam = (pts - t.unsqueeze(0)) @ R_inv  # (N, 3)
+        points_cam = pts
         K = K.to(device)
         points_h = points_cam @ K.transpose(0, 1)  # (N, 3)
 
