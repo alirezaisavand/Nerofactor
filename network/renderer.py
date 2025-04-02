3cdb28a6d322ca4042853f63cbf3b89e9e68c562
alirezaisavand <isavand.alireza@gmail.com>
2025-04-02 17:09:08 -0600

    debug projection function from 3D to 2D

--- network/renderer.py
+++ network/renderer.py
@@ -1069,7 +1069,7 @@ class NeROMaterialRenderer(nn.Module):
 
         R = pose[:, :3]
         t = pose[:, 3]
-        pts_cam = R.t()@(pts - t)
+        pts_cam = (pts - t)@R
 
 
         # Number of points.
