cbf387aee051ccd6a9ac56b3775fc79bc4271c88
alirezaisavand <isavand.alireza@gmail.com>
2025-04-02 20:38:00 -0600

    return to original model with metallic and roughness

--- network/renderer.py
+++ network/renderer.py
@@ -875,8 +875,8 @@ class NeROShapeRenderer(nn.Module):
             albedo.append(a.cpu().numpy())
 
         return {
-            # 'metallic': np.concatenate(metallic, 0),
-        #         'roughness': np.concatenate(roughness, 0),
+            'metallic': np.concatenate(metallic, 0),
+                'roughness': np.concatenate(roughness, 0),
                 'albedo': np.concatenate(albedo, 0)}
 
 
@@ -1354,10 +1354,10 @@ class NeROMaterialRenderer(nn.Module):
         shade_outputs['rgb_gt'] = rgb_gt
         shade_outputs['loss_rgb'] = self.compute_rgb_loss(shade_outputs['rgb_pr'], shade_outputs['rgb_gt'])
         if self.cfg['reg_mat']:
-            # shade_outputs['loss_mat_reg'] = self.shader_network.material_regularization(
-            #     pts, normals, shade_outputs['metallic'], shade_outputs['roughness'], shade_outputs['albedo'], shade_outputs['spec_brdf'], step)
             shade_outputs['loss_mat_reg'] = self.shader_network.material_regularization(
-                pts, normals, shade_outputs['albedo'], step)
+                pts, normals, shade_outputs['metallic'], shade_outputs['roughness'], shade_outputs['albedo'], shade_outputs['spec_brdf'], step)
+            # shade_outputs['loss_mat_reg'] = self.shader_network.material_regularization(
+            #     pts, normals, shade_outputs['albedo'], step)
         if self.cfg['reg_diffuse_light']:
             shade_outputs['loss_diffuse_light'] = self.compute_diffuse_light_regularization(
                 shade_outputs['diffuse_light'])
@@ -1437,8 +1437,8 @@ class NeROMaterialRenderer(nn.Module):
             albedo.append(a.cpu().numpy())
 
         return {
-            # 'metallic': np.concatenate(metallic, 0),
-        #         'roughness': np.concatenate(roughness, 0),
+            'metallic': np.concatenate(metallic, 0),
+                'roughness': np.concatenate(roughness, 0),
                 'albedo': np.concatenate(albedo, 0)}
 
 
