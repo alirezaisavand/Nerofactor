b396ffccc3b5065003d2f33b61bc8db972e3b399
alirezaisavand <isavand.alireza@gmail.com>
2025-04-02 18:05:54 -0600

    debug projection function from 3D to 2D

--- network/renderer.py
+++ network/renderer.py
@@ -1074,7 +1074,7 @@ class NeROMaterialRenderer(nn.Module):
         # Apply the intrinsic matrix K to the camera coordinates.
         pts_img_hom = pts_cam@K
         print('pts_img_hom shape:', pts_img_hom.shape)
-        pts_img_hom = pts_img_hom / pts_img_hom[:, 2].clone()
+        pts_img_hom = pts_img_hom / pts_img_hom[:, 2:].clone()
         # Perform perspective division to obtain pixel coordinates.
 
         return pts_img_hom.cpu().numpy()
