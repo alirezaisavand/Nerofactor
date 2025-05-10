4c15120bb0dd3bea648f545908ddcc4cc8d082b0
alirezaisavand <isavand.alireza@gmail.com>
2025-05-09 19:04:15 -0600

    mask out rays toward inside the object

--- network/field.py
+++ network/field.py
@@ -3006,10 +3006,7 @@ class MCShadingNetwork(nn.Module):
         pdf = qh / (4.0 * np.pi * m_x * m_y * cos3 * dot_wo_h.squeeze().abs() + eps)
 
         # 7) **Filter out directions with wi·n <= 0**
-
-        normals_expanded = normals.unsqueeze(1).expand(wi.shape[0], wi.shape[1], 3)
-        cos_i = (wi * normals_expanded).sum(dim=2)
-        mask = (cos_i > 0.0).unsqueeze(2).expand(wi.shape[0], wi.shape[1], 3)
+        mask = (wi * normals.unsqueeze(1)).sum(dim=-1) > 0
         print('wi shape:', wi.shape, 'mask shape:', mask.shape, 'wi[mask]:', wi[mask].shape)
         return h[mask], wi[mask], pdf[mask], cos_th[mask], sin_th[mask], cos_phi[mask], sin_phi[mask]
 
