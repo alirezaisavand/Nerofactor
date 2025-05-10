339f21cf5ac3eaa9756376e71c2eee65642444ad
alirezaisavand <isavand.alireza@gmail.com>
2025-05-09 18:39:38 -0600

    mask out rays toward inside the object

--- network/field.py
+++ network/field.py
@@ -3010,6 +3010,7 @@ class MCShadingNetwork(nn.Module):
         normals_expanded = normals.unsqueeze(1).expand(wi.shape[0], wi.shape[1], 3)
         cos_i = (wi * normals_expanded).sum(dim=2)
         mask = cos_i > 0.0
+        print('wi shape:', wi.shape, 'mask shape:', mask.shape, 'wi[mask]:', wi[mask].shape)
         return h[mask], wi[mask], pdf[mask], cos_th[mask], sin_th[mask], cos_phi[mask], sin_phi[mask]
 
     def compute_outgoing_radiance(self, lights: torch.Tensor,
