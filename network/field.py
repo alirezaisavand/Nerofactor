36c2b8df445dcffabaedff24f3acf0205ef93e9c
alirezaisavand <isavand.alireza@gmail.com>
2025-05-16 14:14:06 -0600

    debug shapes

--- network/field.py
+++ network/field.py
@@ -2840,10 +2840,10 @@ class MCShadingNetwork(nn.Module):
         coeff_x = sin_th * cos_phi
         coeff_y = sin_th * sin_phi
         coeff_z = cos_th
-        print('coeff_x shape', coeff_x.shape, x.shape)
-        h = (coeff_x.unsueeze(2) * x.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3) +
-             coeff_y.unsueeze(2) * y.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3) +
-             coeff_z.unsueeze(2) * z.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3))
+        # print('coeff_x shape', coeff_x.shape, x.shape)
+        h = (coeff_x.unsqueeze(2) * x.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3) +
+             coeff_y.unsqueeze(2) * y.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3) +
+             coeff_z.unsqueeze(2) * z.unsqueeze(1).expand(coeff_x.shape[0], coeff_x.shape[1], 3))
 
         dot = (wo.unsqueeze(1) * h).sum(-1, keepdim=True)
         wi = 2 * dot * h - wo.unsqueeze(1)
