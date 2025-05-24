4c9357f2af6937bcc3a521eaddb79ce592ef6513
alirezaisavand <isavand.alireza@gmail.com>
2025-05-24 17:41:55 -0600

    use pca based tangent frames for points

--- network/field.py
+++ network/field.py
@@ -2825,7 +2825,8 @@ class MCShadingNetwork(nn.Module):
         # x = self.get_orthogonal_directions(normals)  # pn,3
         # y = torch.cross(z, x, dim=-1)  # pn,3
         vertices = torch.from_numpy(np.asarray(mesh.vertices))
-        faces = torch.from_numpy(np.asarray(mesh.triangles))
+        faces = torch.from_numpy(np.asarray(mesh.triangles, dtype=np.int64))
+        faces = faces.to(device)  # keep it on CUDA or CPU as you like
         x, y = self.get_tangent_bitangent_via_kdtree(vertices, faces, T, B, pts, normals, tree)
         z = normals
 
