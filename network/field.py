1fd4c4f55e192313471684a69996193c2c71cf10
alirezaisavand <isavand.alireza@gmail.com>
2025-05-25 20:17:35 -0600

    increase min value for mx and my

--- network/field.py
+++ network/field.py
@@ -3301,16 +3301,20 @@ class MCShadingNetwork(nn.Module):
             else:
                 raise NotImplementedError
 
-            mx_ch, my_ch, alpha_ch, F0_ch, kd_ch, ks_ch = self.predict_anisotropic_components(pts + change)
+            # mx_ch, my_ch, alpha_ch, F0_ch, kd_ch, ks_ch = self.predict_anisotropic_components(pts + change)
+            # reg = reg + torch.mean(
+            #     (torch.abs(kd - kd_ch) +
+            #      torch.abs(ks - ks_ch) +
+            #      torch.abs(mx - mx_ch) +
+            #      torch.abs(my - my_ch) +
+            #      torch.abs(alpha - alpha_ch) +
+            #      torch.abs(F0 - F0_ch)) *
+            #     self.cfg['reg_lambda1'],
+            #             dim=1)
             reg = reg + torch.mean(
-                (torch.abs(kd - kd_ch) +
-                 torch.abs(ks - ks_ch) +
-                 torch.abs(mx - mx_ch) +
-                 torch.abs(my - my_ch) +
-                 torch.abs(alpha - alpha_ch) +
-                 torch.abs(F0 - F0_ch)) *
-                self.cfg['reg_lambda1'],
-                        dim=1)
+                torch.abs(mx - my) * self.cfg['reg_lambda1'],
+                dim=1
+            )
         return reg
 
     def material_regularization(self, pts, normals, metallic, roughness, albedo, step):
