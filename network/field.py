fa712fa56384ec05754608a55f6adecaece0d70c
alirezaisavand <isavand.alireza@gmail.com>
2025-06-11 02:02:35 -0600

    debug energy loss

--- network/field.py
+++ network/field.py
@@ -3006,8 +3006,8 @@ class MCShadingNetwork(nn.Module):
         f_s = (k_s_exp * F * pow_in / denom) * mask.unsqueeze(-1)
         spec_brdf = (k_s_exp * F * pow_in_denom * pdf / denom) * mask.unsqueeze(-1)
         spec_weighted = f_s * specular_lights  # (N,M,3)
-        print('spec weighted valid counts:', spec_weighted.sum(dim=1).shape, valid_counts.shape)
         specular = spec_weighted.sum(dim=1) / valid_counts  # (N,3)
+        print('spec BRDF valid counts:', spec_brdf.sum(dim=1).shape, valid_counts.shape)
         f_s_sum = spec_brdf.sum(dim=1) / valid_counts
         # Total radiance
         R = diffuse + specular  # (N,3)
