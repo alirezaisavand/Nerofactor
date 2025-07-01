810bcbcca93986899b5bd17654575ad5e88562dd
alirezaisavand <isavand.alireza@gmail.com>
2025-07-01 16:54:57 -0600

    increase mask_loss_wight in loss.py from 0.01 to 0.1

--- network/loss.py
+++ network/loss.py
@@ -136,7 +136,7 @@ class InitSDFRegLoss(Loss):
 
 class MaskLoss(Loss):
     default_cfg = {
-        'mask_loss_weight': 0.01,
+        'mask_loss_weight': 0.1, #changed here from 0.01 to 0.1
     }
 
     def __init__(self, cfg):
