fed8f2f42d3d845a23baf016a53e49ac023bbd48
alirezaisavand <isavand.alireza@gmail.com>
2025-07-17 01:36:19 -0600

    change database.py to be compatible with NeRF dataset

--- network/metrics.py
+++ network/metrics.py
@@ -33,12 +33,9 @@ def get_key_images(data_pr, keys, h, w):
 
 
 def draw_materials(data_pr, h, w):
-    keys = [
-            'diffuse_albedo', 'diffuse_light', 'diffuse_color', 'specular_albedo', 'specular_light',
-            'specular_color', 'specular_ref', 'metallic', 'roughness', 'occ_prob', 'indirect_light']
-    # keys = ['diffuse_albedo', 'diffuse_light', 'diffuse_color',
-    #         'specular_albedo', 'specular_light', 'specular_color', 'specular_ref',
-    #         'occ_prob', 'indirect_light', 'spec_brdf']
+    keys = ['diffuse_albedo', 'diffuse_light', 'diffuse_color',
+            'specular_albedo', 'specular_light', 'specular_color', 'specular_ref',
+            'metallic', 'roughness', 'occ_prob', 'indirect_light']
     results = get_key_images(data_pr, keys, h, w)
     results = [concat_images_list(*results[0:3]), concat_images_list(*results[3:7]), concat_images_list(*results[7:])]
     return results
@@ -94,21 +91,14 @@ class MaterialRenderMetrics(Loss):
         ssim = structural_similarity(rgb_gt, rgb_pr, win_size=11, channel_axis=2, data_range=255)
         outputs = {'psnr': np.asarray([psnr]), 'ssim': np.asarray([ssim])}
 
-        # additional_keys = ['albedo', 'metallic', 'roughness', 'specular_light', 'specular_color', 'diffuse_light',
-        #                    'diffuse_color']
-        additional_keys = ['tangents', 'bitangents', 'normals',
-                           'kd', 'ks', 'specular_light', 'diffuse_light', 'F0',
-                           'alpha', 'specular_color', 'diffuse_color', 'mx', 'my',
-                           'f_d_sum', 'f_s_sum', 'L_spec']
-        # additional_keys = ['albedo', 'specular_light', 'specular_color', 'diffuse_light',
-        #                    'diffuse_color', 'spec_brdf']
+        additional_keys = ['albedo', 'metallic', 'roughness', 'specular_light', 'specular_color', 'diffuse_light',
+                           'diffuse_color']
         for k in additional_keys:
             img = color_map_backward(data_pr[k].detach().cpu().numpy())
             if img.shape[-1] == 1: img = np.repeat(img, 3, axis=-1)
             imgs.append(img)
 
-        output_imgs = [concat_images_list(*imgs[:5]), concat_images_list(*imgs[5:10]),
-                       concat_images_list(*imgs[10:15]), concat_images_list(*imgs[15:])]
+        output_imgs = [concat_images_list(*imgs[:5]), concat_images_list(*imgs[5:])]
 
         # output image
         data_index = kwargs['data_index']
