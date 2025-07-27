a3e6146aff7d7177714d5d8f70122c3dfa6d204b
alirezaisavand <isavand.alireza@gmail.com>
2025-07-27 11:21:25 -0600

    increase curv loss weight

--- network/loss.py
+++ network/loss.py
@@ -151,8 +151,8 @@ class MaskLoss(Loss):
 
 class CurvLoss(Loss):
     default_cfg = {
-        'curv_loss_weight_begin': 0.001,
-        'curv_loss_weight_end': 0.001,
+        'curv_loss_weight_begin': 0.01,
+        'curv_loss_weight_end': 0.01,
         'curv_weight_decay_begin': 15000,
         'curv_weight_decay_end': 25000,
     }
