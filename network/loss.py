0d687cf94b9b667aec1c4cc462c5b65d9cab3f3f
alirezaisavand <isavand.alireza@gmail.com>
2025-07-30 20:46:57 -0600

    increase occ loss weight during the training from 0.01 to 1

--- network/loss.py
+++ network/loss.py
@@ -92,7 +92,7 @@ class OccLoss(Loss):
         'occ_loss_weight_begin': 0.01,
         'occ_loss_weight_end': 1,
         'occ_weight_decay_begin': 20000,
-        'occ_weight_decay_end': 50000,
+        'occ_weight_decay_end': 80000,
     }
 
     def map_range_val(self, input_val, input_start, input_end, output_start, output_end):
