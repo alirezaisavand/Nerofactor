e82d100cf1b96374f457dd46ed186abdc2c35b61
alirezaisavand <isavand.alireza@gmail.com>
2025-08-17 09:11:35 -0600

    change near,far calculation when using nerf_dataset

--- network/renderer.py
+++ network/renderer.py
@@ -459,8 +459,9 @@ class NeROShapeRenderer(nn.Module):
         # rays_d = torch.sum(dirs[..., None, :] * poses[..., :3], -1)
         # rays_o = poses[:, :, -1].expand(rays_d.shape)
         rays_d = F.normalize(rays_d, dim=-1)
-        near, far = torch.full((rays_o.shape[0], 1), 1.0), torch.full((rays_o.shape[0], 1), 3.0)
-
+        # Todo changed here for removing masks
+        # near, far = torch.full((rays_o.shape[0], 1), 1.0), torch.full((rays_o.shape[0], 1), 3.0)
+        near, far = self.near_far_from_sphere(rays_o, rays_d)
         return rays_o, rays_d, near, far, poses[idxs]  # rn, 3, 4
 
     # def _process_render_ray_batch(self, ray_batch, poses):
