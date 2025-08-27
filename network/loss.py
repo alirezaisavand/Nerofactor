9deedd7561664e70ce1e06e70d3e2465d4375442
alirezaisavand <isavand.alireza@gmail.com>
2025-08-27 06:15:24 -0600

    increase reg_step from 1000 to 3000

--- network/loss.py
+++ network/loss.py
@@ -123,7 +123,7 @@ class InitSDFRegLoss(Loss):
         pass
 
     def __call__(self, data_pr, data_gt, step, *args, **kwargs):
-        reg_step = 1000
+        reg_step = 3000
         small_threshold = 0.1
         large_threshold = 1.05
         if 'sdf_vals' in data_pr and 'sdf_pts' in data_pr and step < reg_step:
