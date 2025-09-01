3123ab0799a0d6124c4c3c40a1c2e78c2487bd97
alirezaisavand <isavand.alireza@gmail.com>
2025-09-01 07:00:27 -0600

    change InitSDFLoss small_threshold from 0.1 to 0.3

--- network/loss.py
+++ network/loss.py
@@ -124,7 +124,7 @@ class InitSDFRegLoss(Loss):
 
     def __call__(self, data_pr, data_gt, step, *args, **kwargs):
         reg_step = 3000
-        small_threshold = 0.1
+        small_threshold = 0.3
         large_threshold = 1.05
         if 'sdf_vals' in data_pr and 'sdf_pts' in data_pr and step < reg_step:
             norm = torch.norm(data_pr['sdf_pts'], dim=-1)
