5e0818cac22ada65d0a184e4e8aa9b84c7ff42f6
alirezaisavand <isavand.alireza@gmail.com>
2025-09-07 16:51:44 -0600

    change test image

--- dataset/database.py
+++ dataset/database.py
@@ -563,8 +563,8 @@ def get_database_split(database: BaseDatabase, split_type='validation'):
         random.seed(6033)
         img_ids = database.get_img_ids().copy()
         # random.shuffle(img_ids)
-        test_ids = img_ids[:1]
-        train_ids = img_ids[1:]
+        test_ids = img_ids[3:4]
+        train_ids = img_ids[:3] + img_ids[4:]
     elif split_type=='test':
         test_ids, train_ids = read_pickle('configs/synthetic_split_128.pkl')
     else:
