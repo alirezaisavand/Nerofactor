5aa2b1160c47d3f2b1c8f918e214b5da85143f92
alirezaisavand <isavand.alireza@gmail.com>
2025-09-17 20:08:51 -0600

    reduce occ loss begin

--- network/loss.py
+++ network/loss.py
@@ -89,7 +89,7 @@ class StdRecorder(Loss):
 class OccLoss(Loss):
     default_cfg = {
         'occ_loss_weight': 1,  # changed here from 0.01 to 1
-        'occ_loss_weight_begin': 1,
+        'occ_loss_weight_begin': 0.001,
         'occ_loss_weight_end': 1,
         'occ_weight_decay_begin': 20000,
         'occ_weight_decay_end': 50000,
