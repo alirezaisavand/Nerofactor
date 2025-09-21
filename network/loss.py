4e66b436aeaeb52b65ccea96742cdf0311652f6d
alirezaisavand <isavand.alireza@gmail.com>
2025-09-21 13:33:37 -0600

    decrease curv loss weight

--- network/loss.py
+++ network/loss.py
@@ -215,8 +215,8 @@ class BGLoss(Loss):
 
 class CurvLoss(Loss):
     default_cfg = {
-        'curv_loss_weight_begin': 1,
-        'curv_loss_weight_end': 1,
+        'curv_loss_weight_begin': 0.1,
+        'curv_loss_weight_end': 0.1,
         'curv_weight_decay_begin': 20000,
         'curv_weight_decay_end': 50000,
     }
