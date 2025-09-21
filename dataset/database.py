0a0e535c433b2dee953240815fa3e331a4a38418
alirezaisavand <isavand.alireza@gmail.com>
2025-09-20 21:45:05 -0600

    add mask for glossyreal dataset

--- dataset/database.py
+++ dataset/database.py
@@ -231,6 +231,7 @@ class GlossyRealDatabase(BaseDatabase):
         mask_name = self.image_names[img_id]
         mask_name = mask_name.split('.')[0]+'.png'
         mask_fname = f'{self.root}/images_{self.max_len}/masks_train/{mask_name}'
+        print(mask_fname)
         mask_img = cv2.imread(mask_fname, cv2.IMREAD_GRAYSCALE)
         # Convert from 0-255 to 0-1 by dividing by 255.
         mask = mask_img.astype(np.float32) / 255.0
