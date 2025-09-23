9a119fd72e44e65e4761cd78d05b3a59e5d7358e
alirezaisavand <isavand.alireza@gmail.com>
2025-09-23 12:51:51 -0600

    change mask loss scheduling

--- network/loss.py
+++ network/loss.py
@@ -159,10 +159,10 @@ class InitSDFRegLoss(Loss):
 
 class MaskLoss(Loss):
     default_cfg = {
-        'mask_loss_weight_begin': 0.1,
-        'mask_loss_weight_end': 1,
+        'mask_loss_weight_begin': 0.3,
+        'mask_loss_weight_end': 0.001,
         'mask_weight_decay_begin': 0,
-        'mask_weight_decay_end': 5000,
+        'mask_weight_decay_end': 3000,
     }
 
     def get_mask_weight(self, step):
