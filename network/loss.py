7d60574d25d32fbb775c2d9186a9fb9bef8247cc
alirezaisavand <isavand.alireza@gmail.com>
2025-09-28 02:26:44 -0600

    set sdf weight to 5

--- network/loss.py
+++ network/loss.py
@@ -122,7 +122,7 @@ class OccLoss(Loss):
 
 class InitSDFRegLoss(Loss):
     default_cfg = {
-        'SDF_loss_weight': 1,
+        'SDF_loss_weight': 5,
     }
     def __init__(self, cfg):
         self.cfg = {**self.default_cfg, **cfg}
