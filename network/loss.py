61d919a38fa08b99f399c14faac2ed3b265b2e0f
alirezaisavand <isavand.alireza@gmail.com>
2025-09-29 17:05:56 -0600

    change nerf render weight scheduling

--- network/loss.py
+++ network/loss.py
@@ -8,35 +8,51 @@ class Loss:
 
 
 class NeRFRenderLoss(Loss):
+
     default_cfg = {
         'render_loss_weight_begin': 1,
         'render_loss_weight_end': 5,
         'render_weight_decay_begin': 0,
-        'render_weight_decay_end': 5000,
+        'render_weight_decay_mid': 5000,
+        'render_weight_decay_end': 10000,
     }
     def __init__(self, cfg):
         self.cfg = {**self.default_cfg, **cfg}
 
 
+    def cosine_weight_schedule(step: int, cfg: dict = default_cfg) -> float:
+        """
+        Cosine up-then-down schedule:
+        - At steps <= begin: weight = begin_value
+        - From begin -> mid: cosine increase to peak_value
+        - From mid -> end:  cosine decrease back to begin_value
+        - After end:        weight = begin_value
+        """
+        w0  = cfg['render_loss_weight_begin']   # initial value
+        wpk = cfg['render_loss_weight_end']     # peak value (at mid)
+        s0  = cfg['render_weight_decay_begin']  # start increasing
+        sm  = cfg['render_weight_decay_mid']    # peak step
+        s1  = cfg['render_weight_decay_end']    # return to initial
 
-    def get_render_weight(self, step):
-        nom = max(step - self.cfg['render_weight_decay_begin'], 0)
-        mx = max(self.cfg['render_loss_weight_end'], self.cfg['render_loss_weight_begin'])
-        mn = min(self.cfg['render_loss_weight_end'], self.cfg['render_loss_weight_begin'])
-        denom = self.cfg['render_weight_decay_end'] - self.cfg['render_weight_decay_begin']
-        nom = min(nom, denom)
-        coef = mx - mn
-        bias = mn
-        rot = 0
-        if self.cfg['render_loss_weight_end'] - self.cfg['render_loss_weight_begin'] > 0:
-            rot = np.pi
-        anneal_weights = (np.cos((nom / denom) * np.pi + rot) + 1) / 2
+        if not (s0 < sm < s1):
+            raise ValueError("Require begin < mid < end for the schedule.")
 
-        return anneal_weights * coef + bias
+        if step <= s0:
+            return w0
+        if step <= sm:
+            # Increase: w0 -> wpk
+            t = (step - s0) / (sm - s0)  # in [0,1]
+            return w0 + 0.5 * (1 - np.cos(np.pi * t)) * (wpk - w0)
+        if step <= s1:
+            # Decrease: wpk -> w0
+            t = (step - sm) / (s1 - sm)  # in [0,1]
+            return wpk - 0.5 * (1 - np.cos(np.pi * t)) * (wpk - w0)
+        return w0
+
 
     def __call__(self, data_pr, data_gt, step, *args, **kwargs):
         outputs = {}
-
+        if step > 
         if 'loss_rgb' in data_pr: outputs['loss_rgb'] = data_pr['loss_rgb'] * self.get_render_weight(step)
         if 'loss_rgb_fine' in data_pr: outputs['loss_rgb_fine'] = data_pr['loss_rgb_fine']
         if 'loss_global_rgb' in data_pr: outputs['loss_global_rgb'] = data_pr['loss_global_rgb']
