2a0abfb660849aa1631bf43d919209edfe3c5eb9
alirezaisavand <isavand.alireza@gmail.com>
2025-09-29 21:08:12 -0600

    add loss_sdf_small to loss.py

--- network/loss.py
+++ network/loss.py
@@ -195,7 +195,7 @@ class InitSDFRegLoss(Loss):
 
             anneal_weights = (np.cos((step / reg_step) * np.pi) + 1) / 2
             anneal_weights = anneal_weights * self.cfg['SDF_loss_weight']
-            return {'loss_sdf_large': large_loss * anneal_weights, 'loss_sdf_small': small_loss * 0}
+            return {'loss_sdf_large': large_loss * anneal_weights, 'loss_sdf_small': small_loss}
         else:
             return {}
 
