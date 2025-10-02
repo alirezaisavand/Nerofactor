7631d10e348c29c9d9f06ed782331dfd07c45183
alirezaisavand <isavand.alireza@gmail.com>
2025-10-02 01:42:22 -0600

    decrease maskloss weight

--- network/loss.py
+++ network/loss.py
@@ -206,7 +206,7 @@ class InitSDFRegLoss(Loss):
 class MaskLoss(Loss):
     default_cfg = {
         'mask_loss_weight_begin': 0.3,
-        'mask_loss_weight_end': 1,
+        'mask_loss_weight_end': 0.3,
         'mask_weight_decay_begin': 0,
         'mask_weight_decay_end': 30000,
     }
