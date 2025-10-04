123ba752ba71acfab674a54bf082d5eb2fd362db
alirezaisavand <isavand.alireza@gmail.com>
2025-10-03 18:28:01 -0600

    change mask from binary to float

--- dataset/database.py
+++ dataset/database.py
@@ -230,7 +230,7 @@ class GlossyRealDatabase(BaseDatabase):
     def get_mask(self, img_id):
         mask_name = self.image_names[img_id]
         mask_name = mask_name.split('.')[0]+'.png'
-        mask_fname = f'{self.root}/images_{self.max_len}/masks_train/weighted/{mask_name}'
+        mask_fname = f'{self.root}/images_{self.max_len}/masks_train/{mask_name}'
         print(mask_fname)
         mask_img = cv2.imread(mask_fname, cv2.IMREAD_GRAYSCALE)
         # Convert from 0-255 to 0-1 by dividing by 255.
