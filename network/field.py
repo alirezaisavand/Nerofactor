95faf588f245ab08e054883f32ccc6e33b98e728
alirezaisavand <isavand.alireza@gmail.com>
2025-10-22 13:31:49 -0600

    add smoothness prior on sources

--- network/field.py
+++ network/field.py
@@ -1928,7 +1928,7 @@ class MCShadingNetwork(nn.Module):
                     torch.abs(my - my_ch) +
                     torch.abs(alpha - alpha_ch) +
                     torch.abs(F0 - F0_ch)+
-                    # ((sources-sources_ch)**2).sum(dim=-1, keepdim=True)+
+                    ((sources-sources_ch)**2).sum(dim=-1, keepdim=True)+
                     alignment_loss
                     ) *
                 self.cfg['reg_lambda1'],
