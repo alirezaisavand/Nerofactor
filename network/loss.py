cb546f07fb1c9b8a5c71733d7faa39ae23e7f785
alirezaisavand <isavand.alireza@gmail.com>
2025-10-22 15:20:01 -0600

    remove mask loss

--- network/loss.py
+++ network/loss.py
@@ -205,8 +205,8 @@ class InitSDFRegLoss(Loss):
 
 class MaskLoss(Loss):
     default_cfg = {
-        'mask_loss_weight_begin': 0,
-        'mask_loss_weight_end': 0,
+        'mask_loss_weight_begin': 0.3,
+        'mask_loss_weight_end': 1,
         'mask_weight_decay_begin': 0,
         'mask_weight_decay_end': 30000,
     }
