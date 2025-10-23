5dbdde9246043143f3c2c10275472e699c2de3ce
alirezaisavand <isavand.alireza@gmail.com>
2025-10-22 22:37:13 -0600

    remove curv loss

--- network/field.py
+++ network/field.py
@@ -1932,8 +1932,8 @@ class MCShadingNetwork(nn.Module):
                     torch.abs(my - my_ch) +
                     torch.abs(alpha - alpha_ch) +
                     torch.abs(F0 - F0_ch)+
-                    alignment_loss +
-                    curvature_loss
+                    alignment_loss 
+                    # curvature_loss
                     ) *
                 self.cfg['reg_lambda1'],
                         dim=1)
