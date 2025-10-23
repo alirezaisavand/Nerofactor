36380a88a2912d16ad5b0d05432a3b23f140429d
alirezaisavand <isavand.alireza@gmail.com>
2025-10-23 02:53:55 -0600

    change mx my min value

--- network/field.py
+++ network/field.py
@@ -1208,8 +1208,8 @@ class MCShadingNetwork(nn.Module):
 
     def predict_anisotropic_components(self, pts):
         feats = self.feats_network(pts)
-        mx_min, mx_max = 0.03, 1.0
-        my_min, my_max = 0.03, 1.0
+        mx_min, mx_max = 0.005, 1.0
+        my_min, my_max = 0.005, 1.0
         mx = self.mx_predictor(torch.cat([feats, pts], -1))
         mx = mx_min + (mx_max - mx_min)*mx
         my = self.my_predictor(torch.cat([feats, pts], -1))
