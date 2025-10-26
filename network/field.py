abe177ade48f8132ba149e060a37bf773853dde0
alirezaisavand <isavand.alireza@gmail.com>
2025-10-26 13:07:07 -0600

    debug length loss

--- network/field.py
+++ network/field.py
@@ -1904,8 +1904,8 @@ class MCShadingNetwork(nn.Module):
                     torch.abs(my - my_ch) +
                     torch.abs(alpha - alpha_ch) +
                     torch.abs(F0 - F0_ch) +
-                    curv_loss * 0.01 +
-                    rot_len_loss
+                    curv_loss * 0.001 +
+                    rot_len_loss*0.1
                 ) *
                 self.cfg['reg_lambda1'],
                         dim=1)
