0d6ea15bce65f54e2466c347db967f6db778259b
alirezaisavand <isavand.alireza@gmail.com>
2025-10-27 12:40:51 -0600

    change weighted spec light to masked spec light in visualization

--- network/field.py
+++ network/field.py
@@ -1463,7 +1463,7 @@ class MCShadingNetwork(nn.Module):
         f_s = (k_s_exp * F * pow_in / denom) * mask.unsqueeze(-1)
         spec_brdf = (k_s_exp * F * pow_in_denom * pdf / denom) * mask.unsqueeze(-1)
         weighted_specular_light = (k_s_exp * pow_in_denom * pdf / denom) * mask.unsqueeze(-1) * specular_lights
-        masked_specular_light = (pow_in_denom * pdf / denom) * mask.unsqueeze(-1) * specular_lights
+        masked_specular_light = (k_s_exp * pdf / denom) * mask.unsqueeze(-1) * specular_lights
 
         spec_weighted = f_s * specular_lights  # (N,M,3)
         specular = spec_weighted.sum(dim=1) / valid_counts  # (N,3)
