5fbc2cc1ace535e16df6f9b46cbacff1fc419d1c
alirezaisavand <isavand.alireza@gmail.com>
2025-10-26 20:17:02 -0600

    add non_zero_len loss

--- network/renderer.py
+++ network/renderer.py
@@ -1639,7 +1639,7 @@ class NeROMaterialRenderer(nn.Module):
             shade_outputs['loss_mat_reg'] = self.shader_network.anisotropic_regularization(
                 pts, normals,  shade_outputs['sources']*2.0-1.0, shade_outputs['tangents'], shade_outputs['bitangents'], shade_outputs['mx'], shade_outputs['my'], shade_outputs['alpha'], shade_outputs['F0'],
                 shade_outputs['kd'], shade_outputs['ks'], shade_outputs['f_d_sum'], shade_outputs['f_s_sum'],
-                shade_outputs['L_spec'], shade_outputs['rotation', shade_outputs['rotation_raw']], step
+                shade_outputs['L_spec'], shade_outputs['rotation'], shade_outputs['rotation_raw'], step
             )
             # shade_outputs['loss_mat_reg'] = self.shader_network.material_regularization(
             #     pts, normals, shade_outputs['albedo'], step)
