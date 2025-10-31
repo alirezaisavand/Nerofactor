fb5313b6f63530f62ccda55759ab31ce26286308
alirezaisavand <isavand.alireza@gmail.com>
2025-10-30 20:58:44 -0600

    debug final evaluation step

--- network/renderer.py
+++ network/renderer.py
@@ -1240,7 +1240,7 @@ class NeROMaterialRenderer(nn.Module):
             self.test_imgs_info = build_imgs_info(self.database, self.test_ids, self.is_nerf)
             self.test_imgs_info = imgs_info_to_torch(self.test_imgs_info, 'cpu')
             self.nvs_imgs_info = build_imgs_info(self.database, self.nvs_ids, self.is_nerf)
-            self.nvs_imgs_info = imgs_info_to_torch(self.nvs_imgs_info, 'cpu')
+            self.nvs_imgs_info = imgs_info_to_torch(self.nvs_imgs_info, 'cuda')
             self.test_num = len(self.test_ids)
 
             self.train_batch = self._construct_nerf_ray_batch(
