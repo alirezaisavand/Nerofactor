76202c7538b04574839de9b9c6aa3cf08ca9da54
alirezaisavand <isavand.alireza@gmail.com>
2025-11-01 04:49:46 -0600

    update run_all_tonga.sh

--- run_all_tonga.sh
+++ run_all_tonga.sh
@@ -29,8 +29,8 @@ wait_for_gpu() {
 # --- [CONFIGS TO RUN] ---------------------------------------------------------
 # List ONLY the suffix after "configs/material/"
 CONFIGS=(
-  "syn/tbell.yaml"
-  "syn/teapot.yaml"
+  "real/maneki.yaml"
+  "real/vase.yaml"
 )
 
 # You can also auto-discover with a glob, e.g.:
