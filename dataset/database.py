1c24f2e66a4ce77a3b06a649efbef4044bf487b7
alirezaisavand <isavand.alireza@gmail.com>
2025-10-31 19:34:42 -0600

    add eval_nerf_shape.py

--- dataset/database.py
+++ dataset/database.py
@@ -551,7 +551,9 @@ class NeRFSyntheticDatabase(BaseDatabase):
 
     def get_depth(self, img_id):
         assert (self.scale_factor == 1.0)
-        depth_name = f"Image{'0' * (4 - len(str(img_id)))}{str(img_id+1)}.png"
+        str_id = str(img_id + 1)
+        prefix = '0' * (4 - len(str_id))
+        depth_name = "Image"+prefix + str_id + ".png"
         depth = imread(os.path.join(self.root, depth_name))
         depth = depth.astype(np.float32) / 65535 * 15
         mask = depth < 14.5
