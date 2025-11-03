573b2733546d5bfe3c3e898c570524266002694c
alirezaisavand <isavand.alireza@gmail.com>
2025-11-03 00:46:17 -0600

    increase mask loss weight

--- network/loss.py
+++ network/loss.py
@@ -10,7 +10,7 @@ class Loss:
 class NeRFRenderLoss(Loss):
     default_cfg = {
         'render_loss_weight_begin': 1,
-        'render_loss_weight_end': 5,
+        'render_loss_weight_end': 1,
         'render_weight_decay_begin': 0,
         'render_weight_decay_mid': 5000,
         'render_weight_decay_end': 10000,
@@ -160,7 +160,7 @@ class OccLoss(Loss):
 
 class InitSDFRegLoss(Loss):
     default_cfg = {
-        'SDF_loss_weight': 3,
+        'SDF_loss_weight': 1,
     }
 
     def __init__(self, cfg):
@@ -204,8 +204,8 @@ class InitSDFRegLoss(Loss):
 
 class MaskLoss(Loss):
     default_cfg = {
-        'mask_loss_weight_begin': 1,
-        'mask_loss_weight_end': 1,
+        'mask_loss_weight_begin': 2,
+        'mask_loss_weight_end': 2,
         'mask_weight_decay_begin': 0,
         'mask_weight_decay_end': 30000,
     }
